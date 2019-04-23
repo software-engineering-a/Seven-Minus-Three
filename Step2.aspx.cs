887a63c40c0b6a50cc998cee937c437245d0682c
ayeshahaider <45331923+ayeshahaider@users.noreply.github.com>
2019-04-23 06:00:54 -0700

    Add files via upload

---
+++ Step2.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Resume
+{
+    public partial class Step2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Calendar2.Visible = false;
+            }
+        }
+
+        protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Step3.aspx");
+        }
+
+        protected void Calendar2_SelectionChanged(object sender, EventArgs e)
+        {
+            TextBox3.Text = Calendar2.SelectedDate.ToString("dd/MM/yyyy");
+            Calendar2.Visible = false;
+        }
+
+        protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+        {
+            
+            if (Calendar2.Visible)
+            {
+                Calendar2.Visible = false;
+            }
+
+            else
+            {
+                Calendar2.Visible = true;
+            }
+
+            Calendar2.Attributes.Add("style", "possition:absolute");
+    
+        }
+    }
+}
